898ec1c1e56921c84059902c1e2ad73b38395861
1EC4 <nrieske@alumni.ubc.ca>
2021-05-19 18:38:59 -0700

    Implemented the Sieve of Sundaram

---
+++ sundaram.py
@@ -0,0 +1,30 @@
+from math import floor
+
+def sundaram(N):
+    flags = [True] * (N + 1)
+    primes = [2]
+
+    for j in range(1, N + 1):
+        for i in range(1, j + 1):
+            k = i + j + 2 * i * j
+            if k <= N:
+                flags[k] = False
+    
+    for i in range(1, floor((N - 1) / 2) + 1):
+        if flags[i]:
+            primes.append(2 * i + 1)
+
+    return primes
+
+def test_sundaram():
+    if sundaram(10) == [2, 3, 5, 7]:
+        print("Test 1: passed")
+    else:
+        print("Test 1: failed")
+    
+    if sundaram(30) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]:
+        print("Test 2: passed")
+    else:
+        print("Test 2: failed")
+
+test_sundaram()
